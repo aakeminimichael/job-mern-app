3082f1c4eb815c266e0010b424aa01e415688de7
Michael <inyangaakemini@gmail.com>
2026-05-19 15:03:36 +0100

    changed the frontend url to the backend server urlthe userController.js file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "http://18.215.34.4:4000/api/v1/user/getuser",
           {
             withCredentials: true,
           }
