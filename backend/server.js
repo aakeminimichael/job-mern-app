0a6b3167b6eee110f87a72ce2e07d91a6b8e1496
Michael <inyangaakemini@gmail.com>
2026-05-19 13:42:48 +0100

    allowed app internet access

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
